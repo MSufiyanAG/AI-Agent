5130a3646d77087f6eba9c2effa2483764e6a52d
Mohammed Sufiyan Abdullah Ghori <63553397+MSufiyanAG@users.noreply.github.com>
2025-07-25 00:08:07 -0500

    Deployment fixes 1
    Updated the env variables, URL

--- DEPLOY-AI-AGENT/backend/src/api/db.py
+++ DEPLOY-AI-AGENT/backend/src/api/db.py
@@ -2,12 +2,12 @@ import os
 import sqlmodel
 from sqlmodel import Session, SQLModel
 
-DATABASE_URL = os.environ.get('DATBASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL == "":
     raise NotImplementedError("`DATABASE_URL` not implemented")
 
-DATABASE_URL = DATABASE_URL.replace("postgres://", "postgres+psycopg://")
+DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://")
 
 engine = sqlmodel.create_engine(DATABASE_URL)
 
